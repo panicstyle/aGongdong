2dfd210e6597a13b684a89be5a2ab4b076ff60be
dykim <dykim@sumion.com>
2017-03-08 14:48:40 +0900

    InApp 작업중

--- app/src/main/java/com/panicstyle/Gongdong/AboutActivity.java
+++ app/src/main/java/com/panicstyle/Gongdong/AboutActivity.java
@@ -1,15 +1,35 @@
 package com.panicstyle.Gongdong;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
+import com.android.vending.billing.*;
+
 public class AboutActivity extends AppCompatActivity {
+    IInAppBillingService mService;
+
+    ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name,
+                                       IBinder service) {
+            mService = IInAppBillingService.Stub.asInterface(service);
+        }
+    };
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
@@ -27,7 +47,20 @@ public class AboutActivity extends AppCompatActivity {
         String versionInfo = "Version : " + version;
 
         TextView t = (TextView)findViewById(R.id.textVersion); 
-        t.setText(versionInfo);        
-                
+        t.setText(versionInfo);
+
+        Intent serviceIntent =
+                new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mService != null) {
+            unbindService(mServiceConn);
+        }
     }
 }
